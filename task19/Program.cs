d59a254fd1cfd6aca8ebf11e11f8bc22604a702b
Sergey <sergey.malygin.93@mail.ru>
2022-07-02 00:19:12 +0300

    add task 19 "Палиндром"

---
+++ task19/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+if (array[0] == array[4] || array[1] == array[3])
+{
+    Console.WriteLine(string.Join("", array) + "-> да");
+}
+else
+{
+    Console.WriteLine(string.Join("", array) + "-> нет");
+}
+
